e22b9f879b70a9184d3233f5baa0729041e0eb35
romanichh <58612871+romanichh@users.noreply.github.com>
2020-02-28 23:40:11 +0200

    created repository SuperScheduler-mob

---
+++ src/test/java/com/telran/scheduler/manager/ApplicationManager.java
@@ -0,0 +1,43 @@
+package com.telran.scheduler.manager;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ApplicationManager {
+    AppiumDriver driver;
+    SessionHelper session;
+    EventPage event;
+
+    public void init() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "qa22");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
+        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+        capabilities.setCapability("app", "C:/Users/romic/Downloads/v.0.0.3.apk");
+
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        session = new SessionHelper(driver);
+        event = new EventPage(driver);
+    }
+
+    public void stop() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    public SessionHelper getSession() {
+        return session;
+    }
+
+    public EventPage getEvent() {
+        return event;
+    }
+}
---
+++ src/test/java/com/telran/scheduler/tests/FirstTest.java
@@ -0,0 +1,13 @@
+package com.telran.scheduler.tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest extends TestBase {
+
+
+    @Test
+    public void testOpenUp() {
+        System.out.println("App opened");
+    }
+
+}
